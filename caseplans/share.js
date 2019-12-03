d382da41198437ca52b7adfa344b893e467711d6
Gabriel Nicolae <g.nicolae@samsung.com>
2019-12-02 17:31:48 -0800

    Fixed share (and format the code)

--- caseplans/share.js
+++ caseplans/share.js
@@ -3,79 +3,81 @@ var AWS = require('aws-sdk');
 // Set the region
 AWS.config.update({ region: 'us-east-1' });
 
-const planUrl = process.env.PlanUrl
+const planUrl = process.env.PlanUrl;
 
-const getEmailBodyHtml = (code) => `<p>To review your safety plan, please visit:</p>
+const getEmailBodyHtml = code => `<p>To review your safety plan, please visit:</p>
 
 <p><a href="${planUrl}${code}">${planUrl}${code}</a></p>`;
 
-const getEmailBodyText = (code) => `To review your safety plan, please visit:
+const getEmailBodyText = code => `To review your safety plan, please visit:
 
 ${planUrl}${code}`;
 
 // Create sendEmail params
 const getEmailParams = (email, code) => {
-    return {
-        Destination: { /* required */
-            ToAddresses: [
-                email,
-            ]
+  return {
+    Destination: {
+      /* required */
+      ToAddresses: [email]
+    },
+    Message: {
+      /* required */
+      Body: {
+        /* required */
+        Html: {
+          Charset: 'UTF-8',
+          Data: getEmailBodyHtml(code)
         },
-        Message: { /* required */
-            Body: { /* required */
-                Html: {
-                    Charset: "UTF-8",
-                    Data: getEmailBodyHtml(code)
-                },
-                Text: {
-                    Charset: "UTF-8",
-                    Data: getEmailBodyText(code)
-                }
-            },
-            Subject: {
-                Charset: 'UTF-8',
-                Data: 'Your safety plan'
-            }
-        },
-        Source: 'sai.karthik.nadella@pwc.com', /* required */
-        // ReplyToAddresses: [
-        //     'sai.karthik.nadella@pwc.com',
-        //     /* more items */
-        // ],
-    };
-}
+        Text: {
+          Charset: 'UTF-8',
+          Data: getEmailBodyText(code)
+        }
+      },
+      Subject: {
+        Charset: 'UTF-8',
+        Data: 'Your safety plan'
+      }
+    },
+    Source: 'sai.karthik.nadella@pwc.com' /* required */
+    // ReplyToAddresses: [
+    //     'sai.karthik.nadella@pwc.com',
+    //     /* more items */
+    // ],
+  };
+};
 
 exports.shareHandler = async (event, context) => {
-    try {
-        // console.log("event = " + JSON.stringify(event));
-        // console.log("context = " + JSON.stringify(context));
+  try {
+    // console.log("event = " + JSON.stringify(event));
+    // console.log("context = " + JSON.stringify(context));
 
-        bodyText = event['body'] || "{}";
-        body = JSON.parse(bodyText);
-        email = body.email || "";
-        code = body.code || "";
+    bodyText = event['body'] || '{}';
+    body = JSON.parse(bodyText);
+    email = body.email || '';
+    code = body.code || '';
 
+    // Create the promise and SES service object
+    const params = getEmailParams(email, code);
+    const sendPromise = await new AWS.SES({ apiVersion: '2010-12-01' }).sendEmail(params).promise();
+    console.log(JSON.stringify(sendPromise));
 
-        // Create the promise and SES service object
-        const params = getEmailParams(email, code);
-        const sendPromise = await new AWS.SES({ apiVersion: '2010-12-01' }).sendEmail(params).promise();
-        console.log(JSON.stringify(sendPromise));
+    response = {
+      statusCode: 200,
+      body: JSON.stringify({ email, code }),
+      headers: {
+        'Access-Control-Allow-Origin': '*'
+      }
+    };
+  } catch (err) {
+    console.log(err);
+    response = {
+      statusCode: err.statusCode || 500,
+      body: JSON.stringify({ message: err.message }),
+      headers: {
+        'Access-Control-Allow-Origin': '*'
+      }
+    };
+  }
 
-        response = {
-            'statusCode': 200,
-            'body': JSON.stringify({
-                email,
-                code,
-            },
-            'headers': {
-                "Access-Control-Allow-Origin": "*"
-            }
-            )
-        }
-    } catch (err) {
-        console.log(err);
-        return err;
-    }
-
-    return response
+  return response;
 };
